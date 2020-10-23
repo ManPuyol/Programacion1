be95abc534b473acaa48f0fd619b1bbebf3f053d
Manuel Abdiel Ortega Puyol <manuel.ortega@unealum.local>
2020-10-23 11:20:37 +0200

    java

--- ejemploDeSuma.java
+++ ejemploDeSuma.java
@@ -1,11 +1,13 @@
-public class ejemploDeSuma{
+public class ejemploDeSuma {
     public static void main(String[] args) {
-        
+
         int a = 5;
         int b = 7;
         int c = a + b;
 
-        System.out.println(c);     
+        System.out.print("Suma de " + a);
+        System.out.print(" mas " + b);
+        System.out.println(" es " + c);
     }
-    
+
 }
