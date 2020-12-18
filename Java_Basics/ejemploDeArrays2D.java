36a959559d87e161492c04f8169aa006fc9dc8a6
Manuel <manuel.ortega@alumnos.uneatlantico.es>
2020-12-18 12:03:31 +0100

    mapa con antorcha en esteroides

--- Java_Basics/ejemploDeArrays2D.java
+++ Java_Basics/ejemploDeArrays2D.java
@@ -5,38 +5,69 @@ public class ejemploDeArrays2D {
         boolean salirDelJuego = false;
         String opcionDelUsuario;
 
-        int x = 0, y = 0;
-        int[][] unArray2D = { { 0, 1, 3, 3, 0, 0, 0, 0, 0 }, { 0, 1, 2, 3, 0, 0, 0, 0, 0 },
-                { 0, 1, 2, 2, 0, 0, 0, 0, 0 }, { 0, 1, 1, 1, 0, 0, 0, 0, 0 }, { 0, 0, 0, 0, 0, 0, 0, 0, 0 },
-                { 5, 5, 0, 0, 0, 0, 0, 0, 0 }, { 4, 4, 4, 5, 0, 0, 0, 0, 0 }, { 4, 4, 0, 0, 0, 0, 0, 0, 0 },
-                { 4, 0, 0, 0, 0, 0, 0, 0, 0 }, { 0, 0, 0, 0, 0, 0, 0, 0, 0 }, };
+        int x = 0, y = 0, antorcha = 3;
+        int[][] unArray2D = { { 0, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 4, 4, 5, 5 },
+                { 0, 1, 2, 1, 1, 1, 1, 2, 0, 0, 2, 2, 0, 0, 2, 2, 2, 2, 2, 2, 2, 4, 4, 4, 5, 5 },
+                { 0, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 4, 4, 5, 5 },
+                { 0, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2 },
+                { 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 2, 2, 2 },
+                { 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 2, 2, 0, 0, 0, 0, 2 },
+                { 0, 1, 0, 0, 0, 0, 2, 2, 2, 2, 2, 0, 3, 3, 3, 0, 0, 0, 2, 6, 2, 2, 6, 2, 2, 2 },
+                { 0, 0, 0, 0, 0, 1, 1, 4, 2, 2, 2, 3, 3, 2, 2, 4, 0, 0, 2, 6, 2, 2, 6, 1, 0, 1 },
+                { 1, 2, 1, 0, 0, 0, 2, 2, 3, 3, 3, 3, 2, 2, 2, 2, 0, 0, 2, 6, 2, 2, 6, 1, 0, 2 },
+                { 1, 0, 1, 0, 0, 0, 2, 2, 3, 3, 2, 2, 2, 2, 2, 2, 0, 0, 0, 6, 2, 2, 6, 1, 0, 2 },
+                { 1, 0, 1, 0, 0, 0, 2, 4, 3, 2, 2, 2, 2, 2, 2, 4, 0, 0, 0, 6, 2, 2, 6, 1, 0, 2 },
+                { 1, 1, 3, 3, 3, 3, 3, 3, 3, 2, 2, 2, 2, 2, 2, 0, 0, 0, 0, 6, 2, 2, 6, 1, 0, 2 },
+                { 3, 3, 3, 0, 0, 0, 1, 4, 3, 3, 3, 2, 2, 2, 2, 4, 0, 0, 0, 6, 2, 2, 6, 1, 1, 1 },
+                { 1, 0, 1, 0, 0, 0, 0, 3, 3, 3, 3, 3, 2, 2, 2, 2, 0, 0, 0, 6, 2, 2, 6, 2, 2, 2 },
+                { 1, 0, 1, 0, 0, 0, 0, 3, 3, 3, 3, 3, 2, 2, 2, 2, 0, 0, 0, 6, 2, 2, 6, 2, 2, 2 },
+                { 1, 3, 1, 0, 0, 0, 0, 4, 3, 3, 3, 2, 2, 2, 2, 4, 0, 0, 0, 6, 2, 2, 6, 2, 2, 2 },
+                { 1, 1, 1, 0, 0, 0, 0, 0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 6, 1, 1, 1 },
+                { 1, 0, 0, 0, 0, 0, 1, 4, 2, 2, 2, 2, 2, 2, 2, 4, 0, 0, 4, 6, 2, 2, 6, 0, 0, 1 },
+                { 1, 0, 1, 0, 0, 0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 4, 2, 4, 6, 2, 2, 6, 1, 0, 1 },
+                { 1, 1, 1, 0, 0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 4, 4, 4, 6, 2, 2, 6, 1, 0, 1 }, };
 
         do {
-            System.out.println("-------------------------");
+            System.out.print("\033[H\033[2J");
+            System.out.flush();
+            System.out.println("--------------------------------------------------------------------");
         for (int miFila = 0; miFila < unArray2D.length; miFila++) {
             //
             for (int miColumna = 0; miColumna < unArray2D[miFila].length; miColumna++) {
                 if (miFila == y && miColumna == x) {
-                    System.out.print("<>");
-                } else if (unArray2D[miFila][miColumna] == 0) {
-                    System.out.print(" . ");
-                } else if (unArray2D[miFila][miColumna] == 1) {
-                    System.out.print("[#]");
-                } else if (unArray2D[miFila][miColumna] == 2) {
-                    System.out.print(":..");
-                } else if (unArray2D[miFila][miColumna] == 3) {
-                    System.out.print("~~~");
-                } else if (unArray2D[miFila][miColumna] == 4) {
-                    System.out.print(".oO");
-                } else if (unArray2D[miFila][miColumna] == 5) {
-                    System.out.print("0()");
-                } else if (unArray2D[miFila][miColumna] == 6) {
-                    System.out.print(":::");
+                    System.out.print("<M>");
+                    // } else if (Math.abs(miColumna - x) < antorcha && Math.abs(miFila - y) <
+                    // antorcha) {// CONDICION PARA ANTORCHA CUADRADA
+                } else if (Math.pow(miColumna - x, 2) + Math.pow(miFila - y, 2) <= Math.pow(antorcha, 2)) { // CONDICION
+                                                                                                            // PARA
+                                                                                                            // ANTORCHA
+                                                                                                            // CIRCULAR
+                    if (unArray2D[miFila][miColumna] == 0) {
+                        System.out.print(" . ");
+                    } else if (unArray2D[miFila][miColumna] == 1) {
+                        System.out.print("[#]");
+                    } else if (unArray2D[miFila][miColumna] == 2) {
+                        System.out.print(":..");
+                    } else if (unArray2D[miFila][miColumna] == 3) {
+                        System.out.print("~~~");
+                    } else if (unArray2D[miFila][miColumna] == 4) {
+                        System.out.print("*.*");
+                    } else if (unArray2D[miFila][miColumna] == 5) {
+                        System.out.print("*O*");
+                    } else if (unArray2D[miFila][miColumna] == 6) {
+                        System.out.print(":::");
+                    }
+                } else {
+                    System.out.print("???");
                 }
+
             }
             System.out.println("");
         }
-        System.out.println("-------------------------");
+        if (antorcha == 10000) {
+            antorcha = 3;
+        }
+        System.out.println("----------------------------------------------------------------------");
 
         opcionDelUsuario = in.nextLine();
 
@@ -50,10 +81,11 @@ public class ejemploDeArrays2D {
             y++;
         } else if (opcionDelUsuario.equals("d")) {
             x++;
+        } else if (opcionDelUsuario.equals("g")) {
+            antorcha = 10000;
         }
 
     } while (!salirDelJuego);
-
     }
 }
 
